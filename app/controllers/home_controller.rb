042e430d6339652ec9b7179a85aad1cd995e74ec
Carlos Rocha <crocha09.09@gmail.com>
2022-11-14 19:32:29 -0600

    add desing page for dashboard user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
   end
   def dashboard
+    #auth = params[:auth_code]
+    #puts auth
+    #puts '*******************'
+    @user = current_user
     @data = [
       {name: "calories_ingested", data: Calory.unscoped.group_by_day(:date, range: 4.weeks.ago.midnight..Time.now).where(user_id: current_user.id).sum(:calories_ingested)},
       {name: "calories_burned", data: Calory.unscoped.group_by_day(:date, range: 4.weeks.ago.midnight..Time.now).where(user_id: current_user.id).sum(:calories_burned)}
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
   has_many :calories, dependent: :destroy
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def imc
+    weight / (height**2)
+  end
 end
